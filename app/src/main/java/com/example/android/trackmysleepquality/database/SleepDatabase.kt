0c6f4c26143686e7bb9c7f9c9385467e2eb03224
kwambokaB <paula.paulatek@gmail.com>
2022-09-02 17:40:06 +0300

    created Room database instance and associated Data Access Object and data class

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
@@ -15,3 +15,39 @@
  */
 
 package com.example.android.trackmysleepquality.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+// create database class and extend RoomDatabase
+@Database(entities =[SleepNight::class], version = 1, exportSchema = false )
+abstract class SleepDatabase : RoomDatabase(){
+    // associate with DAO
+ abstract val sleepDatabaseDao:SleepDatabaseDao
+
+ // get reference to database
+ companion object {
+     @Volatile
+     private var INSTANCE: SleepDatabase? = null
+
+     fun getInstance(context: Context): SleepDatabase {
+       synchronized(this){
+            var instance = INSTANCE
+           if(instance == null){
+               instance = Room.databaseBuilder(
+                   context.applicationContext,
+                   SleepDatabase::class.java,
+                   "sleep_history_database"
+               )
+                   .fallbackToDestructiveMigration()
+                   .build()
+               INSTANCE= instance
+           }
+
+           return instance
+         }
+     }
+ }
+}
